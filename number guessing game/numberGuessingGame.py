2a702570b2b447a7967d142c8d23987cab57e2b0
smitGaikwad007 <79749328+smitGaikwad007@users.noreply.github.com>
2022-04-29 13:14:42 +0530

    Add files via upload

--- number guessing game/numberGuessingGame.py
+++ number guessing game/numberGuessingGame.py
@@ -6,21 +6,21 @@ print ("geuss a number (between 1 and 9):")
 
 while chances < 5:
 
-    guess = int(input("enter your guess :- "))
+  guess = int(input("enter your guess :- "))
 
 
-    if guess == number:
-      print ("congratulation !! you won !!")
+  if guess == number:
+    print ("congratulation !! you won !!")
     break
 
-    elif guess < number:
-      print ("Your guess was too low :", guess)
+  elif guess < number:
+    print ("Your guess was too low :", guess)
 
-    else :
-      print ("your gues is too high :", guess)
+  else :
+    print ("your gues is too high :", guess)
     
 
-    chances += 1
+  chances += 1
+if not chances < 5 :
 
-    if not chances < 5 :
-      print("you loose")
+  print("you loose")
